3f728bd11c055302dd86958d4c3507298e154b9d
S4uroNN <alin_constantin15@hotmail.com>
2022-08-22 17:57:53 +0200

    tset

---
+++ test.js
@@ -0,0 +1,13 @@
+let list=[7,13,9, 4,1 ,2,16,0];
+
+console.log(list);
+for( let i = list.length - 1; i >= 0;i--){
+    for(let j = 0; j<i ; j++){
+        if(list[j] > list [j+1]){
+            let temp = list[j];
+            list[j] = list [j + 1];
+            list[j+1] =temp;
+        }
+    }
+}
+console.log(list);
