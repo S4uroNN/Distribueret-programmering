6634b701268ad4585e43c0aaaeccdb3d432e769d
S4uroNN <alin_constantin15@hotmail.com>
2022-08-23 11:38:02 +0200

    smth

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let list=[7,13,9, 4,1 ,2,16,0];
 
-console.log("Ikke sorteret: ")
+console.log("Ikke sorteret: "); 
 console.log(list);
 for( let i = list.length - 1; i >= 0;i--){
     for(let j = 0; j<i ; j++){
@@ -11,7 +11,7 @@ for( let i = list.length - 1; i >= 0;i--){
         }
     }
 }
-console.log("Sorteret: ")
+console.log("Sorteret: ");
 console.log(list);
 
 
