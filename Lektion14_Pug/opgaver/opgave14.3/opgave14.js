6c1699b7d06074934d5d535e5693f41154a6059a
S4uroNN <alin_constantin15@hotmail.com>
2022-10-28 08:51:31 +0200

    lots

--- Lektion14_Pug/opgaver/opgave14.3/opgave14.js
+++ Lektion14_Pug/opgaver/opgave14.3/opgave14.js
@@ -5,21 +5,30 @@ const path = require('path');
 const fetch = require('node-fetch');
 
 
-const url = 'https://randomuser.me/api/?nat=dk&results=100'
+const url = 'https://randomuser.me/api/?nat=dk/fr&results=1000'
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '/views'))
 
-const result = get(url).then((value) => value.result)
-    
+
+//Promise function, derfor .then callback
+const results = get(url)
+results.then((result) => {
+    printresult(result)
+});
+
+//Sender afsted til Pug fil, til unpack
+function printresult(results) {
+    const liste = results;
+
+    console.log(pug.renderFile(path.join(__dirname, '/views/users.pug'), liste));
+    app.get('/', (req, response) => response.render('users', liste))
+}
+
+
+app.listen(8000, () => console.log('Test running'))
 async function get(url) {
     const respons = await fetch(url);
     if (respons.status !== 200) // OK
         throw new Error(respons.status);
     return await respons.json();
 }
-
-app.set('view engine', ' pug')
-app.set('views', path.join(__dirname, '/views'));
-
-console.log(pug.renderFile(path.join(__dirname, '/views/pug.pug'), result));
-
-app.get('/', (req, response) => response.render('opgave14', result))
-app.listen(8000, () => console.log('Test running'))
